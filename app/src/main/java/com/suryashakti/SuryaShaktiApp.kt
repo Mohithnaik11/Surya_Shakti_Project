5806a2e165b4bf9215f6da0c63512ab6e45685a3
naikmohit379-lab <naikmohit379@gmail.com>
2026-05-14 13:22:33 +0530

    testing commit

--- app/src/main/java/com/suryashakti/SuryaShaktiApp.kt
+++ app/src/main/java/com/suryashakti/SuryaShaktiApp.kt
@@ -5,6 +5,8 @@ import com.suryashakti.data.AppDatabase
 import com.suryashakti.data.SettingsRepository
 import com.suryashakti.repository.SolarRepository
 
+
+
 class SuryaShaktiApp : Application() {
     val database by lazy { AppDatabase.getDatabase(this) }
     val repository by lazy { SolarRepository(database.solarLogDao()) }
